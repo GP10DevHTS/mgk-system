919270371d2eaf44e16db232c12da782014e6160
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2024-08-15 01:58:39 +0300

    products db

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::redirect('/', '/login');
+Route::redirect('/register', '/login');
 
 Route::middleware([
     'auth:sanctum',
